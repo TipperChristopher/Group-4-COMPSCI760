2a84277e53b1513305fe1c794b3ff0395217841e
ctip297-svg <ctip297@aucklanduni.ac.nz>
2026-09-07 17:59:14 +1200

    evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,7 @@
 import gymnasium as gym
 import f1tenth_gym
 from stable_baselines3 import PPO
-from f1tenth_rl_project.wrappers import F1TenthSB3Wrapper
+from sb3_wrapper import F1TenthSB3Wrapper
 import matplotlib.pyplot as plt
 
 def main():
